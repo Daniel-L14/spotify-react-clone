9159aa7851bf4959f299215a8bfc8f51bfb051a8
EdisonAmadoc <edison.amadoc@uniagustiniana.edu.co>
2026-03-09 15:22:51 +0000

    player2

--- src/componentes/Discografia.jsx
+++ src/componentes/Discografia.jsx
@@ -12,26 +12,34 @@ const Discografia = () => {
     { titulo: "Junto a Ti", año: 2016, tipo: "EP", portada: "https://is1-ssl.mzstatic.com/image/thumb/Music62/v4/ab/4c/24/ab4c24d3-85a9-a35b-c2c6-39aefa3b326e/190394830267.jpg/600x600bf-60.jpg" }
   ];
 
+  const artistas = [
+    { nombre: "Lalo Ayala", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/0d/b4/d8/0db4d8a6-c6c4-63d9-7d16-8c02173b7e79/199891526291_cover.jpg/1200x630bb.jpg" },
+    { nombre: "Rojo Tabaco", imagen: "https://i.scdn.co/image/ab6761610000e5eb5a5b18fcf22acd0ec726389c" },
+    { nombre: "Grupo Enigma", imagen: "https://i.scdn.co/image/ab67616d0000b27349870020d586470e0b5835ee" },
+    { nombre: "Impulso", imagen: "https://i.scdn.co/image/ab67616100005174d67714368e965891c5122812" },
+    { nombre: "Perseguidos", imagen: "https://i.scdn.co/image/ab67616d00001e0202933843035b9b16ce3fa747" },
+  ];
+
   return (
     <>
       {/* Sección Discografía */}
       <section className="discografia">
         <h2 className="titulo-seccion">Discografía</h2>
         <div className="tabs">
-          <button 
-            className={activeTab === "populares" ? "active" : ""} 
+          <button
+            className={activeTab === "populares" ? "active" : ""}
             onClick={() => setActiveTab("populares")}
           >
             Títulos populares
           </button>
-          <button 
-            className={activeTab === "albumes" ? "active" : ""} 
+          <button
+            className={activeTab === "albumes" ? "active" : ""}
             onClick={() => setActiveTab("albumes")}
           >
             Álbumes
           </button>
-          <button 
-            className={activeTab === "sencillos" ? "active" : ""} 
+          <button
+            className={activeTab === "sencillos" ? "active" : ""}
             onClick={() => setActiveTab("sencillos")}
           >
             Sencillos y EP
@@ -39,7 +47,7 @@ const Discografia = () => {
         </div>
 
         {activeTab === "populares" && (
-          <div className="portadas-resumidas">
+          <div className="portadas-resumidas scroll-horizontal">
             {discos.map((disco, index) => (
               <div key={index} className="card-disco">
                 <div className="imagen-container">
@@ -56,7 +64,7 @@ const Discografia = () => {
         )}
 
         {activeTab === "albumes" && (
-          <div className="portadas-resumidas">
+          <div className="portadas-resumidas scroll-horizontal">
             {discos.filter(d => d.tipo === "Álbum").map((disco, index) => (
               <div key={index} className="card-disco">
                 <div className="imagen-container">
@@ -73,7 +81,7 @@ const Discografia = () => {
         )}
 
         {activeTab === "sencillos" && (
-          <div className="portadas-resumidas">
+          <div className="portadas-resumidas scroll-horizontal">
             {discos.filter(d => d.tipo !== "Álbum").map((disco, index) => (
               <div key={index} className="card-disco">
                 <div className="imagen-container">
@@ -96,9 +104,9 @@ const Discografia = () => {
         <div className="portadas-resumidas">
           <div className="card-disco">
             <div className="imagen-container">
-              <img 
-                src="https://pickasso.spotifycdn.com/image/ab67c0de0000deef/dt/v1/img/radio/artist/13Aw4kvMgRhqQQndVGx9dc/en" 
-                alt="Radio de Inicio" 
+              <img
+                src="https://pickasso.spotifycdn.com/image/ab67c0de0000deef/dt/v1/img/radio/artist/13Aw4kvMgRhqQQndVGx9dc/en"
+                alt="Radio de Inicio"
               />
               <div className="overlay">
                 <span className="play-icon"></span>
@@ -111,25 +119,24 @@ const Discografia = () => {
         </div>
       </section>
 
-      {/* Sección Informacion*/}
+      {/* Sección Informacion */}
       <section className="informacion">
         <h2 className="titulo-seccion">Información</h2>
         <div className="portadas-resumidas">
           <div className="card-disco">
             <div className="imagen-container">
-              <img 
-                src="https://scontent-bog2-1.xx.fbcdn.net/v/t1.6435-9/119198435_3196589323787212_623852024896933617_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=06a7ca&_nc_ohc=kqII79KxaccQ7kNvwGBrjwf&_nc_oc=Adm_ydGv06jdoUOuEic9tHO0fmwxVFfZ4ZrneOyKFyoLHfxg7rgWXlPzllBmwSLVqfY&_nc_zt=23&_nc_ht=scontent-bog2-1.xx&_nc_gid=9K3xLD-y-AizMfFVrrlEeg&_nc_ss=8&oh=00_AfzXnZxqc7GueCPjGNCxLJoZx7E54ti41e_x9x8FXN1w2g&oe=69D5E029" 
-                alt="Informacion" 
+              <img
+                src="https://scontent-bog2-1.xx.fbcdn.net/v/t1.6435-9/119198435_3196589323787212_623852024896933617_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=06a7ca&_nc_ohc=kqII79KxaccQ7kNvwGBrjwf&_nc_oc=Adm_ydGv06jdoUOuEic9tHO0fmwxVFfZ4ZrneOyKFyoLHfxg7rgWXlPzllBmwSLVqfY&_nc_zt=23&_nc_ht=scontent-bog2-1.xx&_nc_gid=9K3xLD-y-AizMfFVrrlEeg&_nc_ss=8&oh=00_AfzXnZxqc7GueCPjGNCxLJoZx7E54ti41e_x9x8FXN1w2g&oe=69D5E029"
+                alt="Informacion"
               />
-              {/* Texto dentro de la imagen */}
               <div className="texto-en-imagen">
                 7146 oyentes mensuales
               </div>
             </div>
             <p className="descripcion">
-            En la ciudad de Houston, Texas y contando con la colaboración de varios amigos, 
-            tres hermanos empezaron a poner su sello musical en los oídos del público. La...
-          </p>
+              En la ciudad de Houston, Texas y contando con la colaboración de varios amigos,
+              tres hermanos empezaron a poner su sello musical en los oídos del público. La...
+            </p>
           </div>
         </div>
       </section>
@@ -137,165 +144,90 @@ const Discografia = () => {
       {/* Sección Descubrir */}
       <section className="descubrir">
         <h2 className="titulo-seccion">Dónde se le puede descubrir</h2>
-        <div className="portadas-resumidas">
+        <div className="portadas-resumidas scroll-horizontal">
           <div className="card-disco">
             <div className="imagen-container">
               <img src="https://mosaic.scdn.co/640/ab67616d00001e02614a2780e769e8941a89a47cab67616d00001e02bcf934479fe3f6673f4b1e82ab67616d00001e02c5ea9db967561a4093688e70ab67616d00001e02f7f117cb349340afc17f5990" alt="RGV JaMMz" />
-              <div className="overlay">
-                <span className="play-icon"></span>
-              </div>
+              <div className="overlay"><span className="play-icon"></span></div>
             </div>
-          <h3>RGV JaMMz</h3>
-        <p>De Mario Santos</p>
+            <h3>RGV JaMMz</h3>
+            <p>De Mario Santos</p>
           </div>
-        <div className="card-disco">
-          <div className="imagen-container">
-            <img src="https://mosaic.scdn.co/300/ab67616d00001e0294a79f8dd7e23febd097d078ab67616d00001e02b0a6a73a02e5b7567abcaf7cab67616d00001e02ce9756cbc9333f0f3a45b332ab67616d00001e02d65349dc0f3e5708f0c38d0b" alt="Norteñas Lights" />
-              <div className="overlay">
-                  <span className="play-icon"></span>
-                </div>
-              </div>
+          <div className="card-disco">
+            <div className="imagen-container">
+              <img src="https://mosaic.scdn.co/300/ab67616d00001e0294a79f8dd7e23febd097d078ab67616d00001e02b0a6a73a02e5b7567abcaf7cab67616d00001e02ce9756cbc9333f0f3a45b332ab67616d00001e02d65349dc0f3e5708f0c38d0b" alt="Norteñas Lights" />
+              <div className="overlay"><span className="play-icon"></span></div>
+            </div>
             <h3>Norteñas Lights</h3>
             <p>De Isaac</p>
           </div>
           <div className="card-disco">
             <div className="imagen-container">
               <img src="https://mosaic.scdn.co/300/ab67616d00001e0204c84b693b2369d9ef570a12ab67616d00001e0243acbc37a870d1f02659e461ab67616d00001e025db4c3fbd6c896839053cb5dab67616d00001e02b17b789599c6dc55b51a323c" alt="Platos de segunda mesa" />
-                <div className="overlay">
-                <span className="play-icon"></span>
-              </div>
+              <div className="overlay"><span className="play-icon"></span></div>
             </div>
-          <h3>Platos de segunda mesa con los Tigres del Norte</h3>
-          <p>De Aleca</p>
-        </div>
-        <div className="card-disco">
-          <div className="imagen-container">
-            <img src="https://i.scdn.co/image/ab67616d0000b27393980977592454831d4bbf2d" alt="Tejano Norteño Light" />
-            <div className="overlay">
-              <span className="play-icon"></span>
-              </div>
-            </div>
-          <h3>Tejano Norteño Light</h3>
-          <p>Tejano Norteño Light by Checo Dj</p>
-        </div>
-        <div className="card-disco">
-          <div className="imagen-container">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBapR42HCz6wetB3Qwuk08Oh-nHAR4xatwBQ&s" alt="Puro acordeón, bajo sexto y sax" />
-              <div className="overlay">
-                <span className="play-icon"></span>
-              </div>
-            </div>
-          <h3>Puro acordeón, bajo sexto y sax</h3>
-          <p>De Omar Mndz</p>
-        </div>
-    </div>
-  </section>
-
-  {/* Sección Sus fans también escuchan */}
-  <section className="artistas">
-    <h2 className="titulo-seccion">Sus fans también escuchan</h2>
-    <div className="portadas-resumidas">
-      <div className="card-disco">
-        <div className="imagen-container">
-          <img 
-            src="https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/0d/b4/d8/0db4d8a6-c6c4-63d9-7d16-8c02173b7e79/199891526291_cover.jpg/1200x630bb.jpg" 
-            alt="Artista 1" 
-            className="imagen-artista"
-          />
-          <div className="overlay">
-            <span className="play-icon"></span>
+            <h3>Platos de segunda mesa con los Tigres del Norte</h3>
+            <p>De Aleca</p>
           </div>
-        </div>
-        <h3>Lalo Ayala</h3>
-        <p>Artista</p>
-      </div>
-
-      <div className="card-disco">
-        <div className="imagen-container">
-          <img 
-            src="https://i.scdn.co/image/ab6761610000e5eb5a5b18fcf22acd0ec726389c" 
-            alt="Artista 2" 
-            className="imagen-artista"
-          />
-          <div className="overlay">
-            <span className="play-icon"></span>
-          </div>
-        </div>
-        <h3>Rojo Tabaco</h3>
-        <p>Artista</p>
-      </div>
-
-      <div className="card-disco">
-        <div className="imagen-container">
-          <img 
-            src="https://i.scdn.co/image/ab67616d0000b27349870020d586470e0b5835ee" 
-            alt="Artista 3" 
-            className="imagen-artista"
-          />
-          <div className="overlay">
-            <span className="play-icon"></span>
-          </div>
-        </div>
-        <h3>Grupo Enigma</h3>
-        <p>Artista</p>
-      </div>
-
-      <div className="card-disco">
-        <div className="imagen-container">
-          <img 
-            src="https://i.scdn.co/image/ab67616100005174d67714368e965891c5122812" 
-            alt="Artista 4" 
-            className="imagen-artista"
-          />
-          <div className="overlay">
-            <span className="play-icon"></span>
-          </div>
-        </div>
-        <h3>Impulso</h3>
-        <p>Artista</p>
-      </div>
-
-      <div className="card-disco">
-        <div className="imagen-container">
-          <img 
-            src="https://i.scdn.co/image/ab67616d00001e0202933843035b9b16ce3fa747" 
-            alt="Artista 5" 
-            className="imagen-artista"
-          />
-          <div className="overlay">
-            <span className="play-icon"></span>
-          </div>
-        </div>
-        <h3>Perseguidos</h3>
-        <p>Artista</p>
-      </div>
-    </div>
-  </section>
-
-  {/* Sección Aparece en*/}
-      <section className="apareceen">
-        <h2 className="titulo-seccion">Aparece en</h2>
-        <div className="portadas-resumidas">
           <div className="card-disco">
             <div className="imagen-container">
-              <img 
-                src="https://i.scdn.co/image/ab67616d00001e02d9be2b4d407d0f7957c1d568" 
-                alt="Aparece en" 
+              <img src="https://i.scdn.co/image/ab67616d0000b27393980977592454831d4bbf2d" alt="Tejano Norteño Light" />
+              <div className="overlay"><span className="play-icon"></span></div>
+            </div>
+            <h3>Tejano Norteño Light</h3>
+            <p>Tejano Norteño Light by Checo Dj</p>
+          </div>
+          <div className="card-disco">
+            <div className="imagen-container">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBapR42HCz6wetB3Qwuk08Oh-nHAR4xatwBQ&s" alt="Puro acordeón, bajo sexto y sax" />
+              <div className="overlay"><span className="play-icon"></span></div>
+            </div>
+            <h3>Puro acordeón, bajo sexto y sax</h3>
+            <p>De Omar Mndz</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Sección Sus fans también escuchan */}
+      <section className="artistas">
+        <h2 className="titulo-seccion">Sus fans también escuchan</h2>
+        <div className="portadas-resumidas scroll-horizontal">
+          {artistas.map((artista, index) => (
+            <div key={index} className="card-disco">
+              <div className="imagen-container imagen-redonda">
+                <img src={artista.imagen} alt={artista.nombre} className="imagen-artista" />
+                <div className="overlay">
+                  <span className="play-icon"></span>
+                </div>
+              </div>
+              <h3>{artista.nombre}</h3>
+              <p>Artista</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Sección Aparece en */}
+      <section className="apareceen">
+        <h2 className="titulo-seccion">Aparece en</h2>
+        <div className="portadas-resumidas scroll-horizontal">
+          <div className="card-disco">
+            <div className="imagen-container">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e02d9be2b4d407d0f7957c1d568"
+                alt="Aparece en"
               />
               <div className="overlay">
                 <span className="play-icon"></span>
               </div>
             </div>
-          <h3>MixTape Preview 3</h3>
-          <p>2021 · Álbum</p>
-          <p> </p>
-          <p> </p>
-          <p> </p>
+            <h3>MixTape Preview 3</h3>
+            <p>2021 · Álbum</p>
+          </div>
         </div>
-      </div>
-    </section>
-</>
-);
+      </section>
+    </>
+  );
 };
 
 export default Discografia;
--- src/componentes/Player.jsx
+++ src/componentes/Player.jsx
@@ -8,7 +8,11 @@ const Player = () => {
   const [isShuffle, setIsShuffle] = useState(false);
   const [isRepeat, setIsRepeat] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const progressRef = useRef(null);
+
+  // Imagen del artista — cámbiala por la que necesites
+  const COVER_URL = "https://i.scdn.co/image/ab6761610000e5eb5a5b18fcf22acd0ec726389c";
 
   useEffect(() => {
     let interval;
@@ -34,98 +38,152 @@ const Player = () => {
   };
 
   return (
-    <div className="sp-player">
+    <div className={`sp-player ${isCollapsed ? 'sp-collapsed' : ''}`}>
 
-      {/* LEFT */}
-      <div className="sp-track">
-        <div className="sp-cover">🎵</div>
-        <div className="sp-track-meta">
-          <div className="sp-track-name">Canción Actual</div>
-          <div className="sp-track-artist">Artista</div>
-        </div>
-        <button
-          className={`sp-like-btn ${isLiked ? 'liked' : ''}`}
-          onClick={() => setIsLiked(!isLiked)}
-          title={isLiked ? 'Quitar de favoritos' : 'Guardar en favoritos'}
-        >
-          <svg viewBox="0 0 24 24" fill={isLiked ? 'currentColor' : 'none'} stroke="currentColor" strokeWidth="2">
-            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
-          </svg>
-        </button>
+      {/* Línea de progreso visible en mini */}
+      <div className="sp-top-progress">
+        <div className="sp-top-fill" style={{ width: `${progress}%` }} />
       </div>
 
-      {/* CENTER */}
-      <div className="sp-controls">
-        <div className="sp-btns">
-          <button className={`sp-ctrl-btn ${isShuffle ? 'active' : ''}`} onClick={() => setIsShuffle(!isShuffle)} title="Aleatorio">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <polyline points="16 3 21 3 21 8"/><line x1="4" y1="20" x2="21" y2="3"/>
-              <polyline points="21 16 21 21 16 21"/><line x1="15" y1="15" x2="21" y2="21"/>
-            </svg>
-          </button>
-          <button className="sp-ctrl-btn" title="Anterior">
-            <svg viewBox="0 0 24 24" fill="currentColor">
-              <path d="M6 6h2v12H6zm3.5 6l8.5 6V6z"/>
-            </svg>
-          </button>
-          <button className="sp-play-btn" onClick={() => setIsPlaying(!isPlaying)} title={isPlaying ? 'Pausar' : 'Reproducir'}>
+      {/* ── MODO MINI (colapsado) ── */}
+      {isCollapsed && (
+        <div className="sp-mini">
+          <img className="sp-mini-cover" src={COVER_URL} alt="cover" />
+          <div className="sp-mini-meta">
+            <span className="sp-mini-name">Canción Actual</span>
+            <span className="sp-mini-artist">Artista</span>
+          </div>
+          <button className="sp-play-btn" onClick={() => setIsPlaying(!isPlaying)}>
             {isPlaying
               ? <svg viewBox="0 0 24 24" fill="currentColor"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
               : <svg viewBox="0 0 24 24" fill="currentColor"><path d="M8 5v14l11-7z"/></svg>
             }
           </button>
-          <button className="sp-ctrl-btn" title="Siguiente">
-            <svg viewBox="0 0 24 24" fill="currentColor">
-              <path d="M6 18l8.5-6L6 6v12zm2-8.14L11.03 12 8 14.14V9.86zM16 6h2v12h-2z"/>
+          <button
+            className="sp-like-btn"
+            onClick={() => setIsLiked(!isLiked)}
+          >
+            <svg viewBox="0 0 24 24" fill={isLiked ? 'currentColor' : 'none'} stroke="currentColor" strokeWidth="2">
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
             </svg>
           </button>
-          <button className={`sp-ctrl-btn ${isRepeat ? 'active' : ''}`} onClick={() => setIsRepeat(!isRepeat)} title="Repetir">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <polyline points="17 1 21 5 17 9"/><path d="M3 11V9a4 4 0 0 1 4-4h14"/>
-              <polyline points="7 23 3 19 7 15"/><path d="M21 13v2a4 4 0 0 1-4 4H3"/>
+          <button className="sp-collapse-btn" onClick={() => setIsCollapsed(false)} title="Expandir">
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round">
+              <polyline points="18 15 12 9 6 15"/>
             </svg>
           </button>
         </div>
+      )}
 
-        <div className="sp-progress-wrap">
-          <span className="sp-time">{formatTime(progress)}</span>
-          <div className="sp-bar" ref={progressRef} onClick={handleProgressClick}>
-            <div className="sp-fill" style={{ width: `${progress}%` }}>
-              <div className="sp-handle"/>
+      {/* ── MODO COMPLETO (expandido) ── */}
+      {!isCollapsed && (
+        <div className="sp-full">
+
+          {/* LEFT */}
+          <div className="sp-track">
+            <div className="sp-cover-wrap">
+              <img
+                className="sp-cover-img"
+                src={COVER_URL}
+                alt="Portada"
+                style={{ animationPlayState: isPlaying ? 'running' : 'paused' }}
+              />
+            </div>
+            <div className="sp-track-meta">
+              <div className="sp-track-name">Canción Actual</div>
+              <div className="sp-track-artist">Artista</div>
+            </div>
+            <button
+              className={`sp-like-btn ${isLiked ? 'liked' : ''}`}
+              onClick={() => setIsLiked(!isLiked)}
+            >
+              <svg viewBox="0 0 24 24" fill={isLiked ? 'currentColor' : 'none'} stroke="currentColor" strokeWidth="2">
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
+              </svg>
+            </button>
+          </div>
+
+          {/* CENTER */}
+          <div className="sp-controls">
+            <div className="sp-btns">
+              <button className={`sp-ctrl-btn ${isShuffle ? 'active' : ''}`} onClick={() => setIsShuffle(!isShuffle)} title="Aleatorio">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <polyline points="16 3 21 3 21 8"/><line x1="4" y1="20" x2="21" y2="3"/>
+                  <polyline points="21 16 21 21 16 21"/><line x1="15" y1="15" x2="21" y2="21"/>
+                </svg>
+              </button>
+              <button className="sp-ctrl-btn" title="Anterior">
+                <svg viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M6 6h2v12H6zm3.5 6 8.5 6V6z"/>
+                </svg>
+              </button>
+              <button className="sp-play-btn" onClick={() => setIsPlaying(!isPlaying)}>
+                {isPlaying
+                  ? <svg viewBox="0 0 24 24" fill="currentColor"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
+                  : <svg viewBox="0 0 24 24" fill="currentColor"><path d="M8 5v14l11-7z"/></svg>
+                }
+              </button>
+              <button className="sp-ctrl-btn" title="Siguiente">
+                <svg viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M6 18l8.5-6L6 6v12zm2-8.14L11.03 12 8 14.14V9.86zM16 6h2v12h-2z"/>
+                </svg>
+              </button>
+              <button className={`sp-ctrl-btn ${isRepeat ? 'active' : ''}`} onClick={() => setIsRepeat(!isRepeat)} title="Repetir">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <polyline points="17 1 21 5 17 9"/><path d="M3 11V9a4 4 0 0 1 4-4h14"/>
+                  <polyline points="7 23 3 19 7 15"/><path d="M21 13v2a4 4 0 0 1-4 4H3"/>
+                </svg>
+              </button>
+            </div>
+
+            <div className="sp-progress-wrap">
+              <span className="sp-time">{formatTime(progress)}</span>
+              <div className="sp-bar" ref={progressRef} onClick={handleProgressClick}>
+                <div className="sp-fill" style={{ width: `${progress}%` }}>
+                  <div className="sp-handle"/>
+                </div>
+              </div>
+              <span className="sp-time">3:34</span>
             </div>
           </div>
-          <span className="sp-time">3:34</span>
-        </div>
-      </div>
 
-      {/* RIGHT */}
-      <div className="sp-right">
-        <button className="sp-ctrl-btn" title="Cola">
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round">
-            <line x1="8" y1="6" x2="21" y2="6"/><line x1="8" y1="12" x2="21" y2="12"/>
-            <line x1="8" y1="18" x2="21" y2="18"/><line x1="3" y1="6" x2="3.01" y2="6"/>
-            <line x1="3" y1="12" x2="3.01" y2="12"/><line x1="3" y1="18" x2="3.01" y2="18"/>
-          </svg>
-        </button>
-        <div className="sp-vol-wrap">
-          <button className="sp-ctrl-btn" title="Volumen">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round">
-              <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/>
-              <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"/>
-            </svg>
-          </button>
-          <div
-            className="sp-vol-bar"
-            onClick={(e) => {
-              const rect = e.currentTarget.getBoundingClientRect();
-              setVolume(Math.round(((e.clientX - rect.left) / rect.width) * 100));
-            }}
-          >
-            <div className="sp-vol-fill" style={{ width: `${volume}%` }}/>
+          {/* RIGHT */}
+          <div className="sp-right">
+            <button className="sp-ctrl-btn" title="Cola">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round">
+                <line x1="8" y1="6" x2="21" y2="6"/><line x1="8" y1="12" x2="21" y2="12"/>
+                <line x1="8" y1="18" x2="21" y2="18"/><line x1="3" y1="6" x2="3.01" y2="6"/>
+                <line x1="3" y1="12" x2="3.01" y2="12"/><line x1="3" y1="18" x2="3.01" y2="18"/>
+              </svg>
+            </button>
+            <div className="sp-vol-wrap">
+              <button className="sp-ctrl-btn" title="Volumen">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round">
+                  <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/>
+                  <path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"/>
+                </svg>
+              </button>
+              <div
+                className="sp-vol-bar"
+                onClick={(e) => {
+                  const rect = e.currentTarget.getBoundingClientRect();
+                  setVolume(Math.round(((e.clientX - rect.left) / rect.width) * 100));
+                }}
+              >
+                <div className="sp-vol-fill" style={{ width: `${volume}%` }}/>
+              </div>
+            </div>
+
+            {/* Botón colapsar */}
+            <button className="sp-collapse-btn" onClick={() => setIsCollapsed(true)} title="Minimizar">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round">
+                <polyline points="6 9 12 15 18 9"/>
+              </svg>
+            </button>
           </div>
-        </div>
-      </div>
 
+        </div>
+      )}
     </div>
   );
 };
